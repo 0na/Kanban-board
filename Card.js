20e94091f594582006fa365c472f47ea84b10048
0na <41240976+0na@users.noreply.github.com>
2019-02-03 11:14:14 +0100

    Add files via upload

---
+++ Card.js
@@ -0,0 +1,45 @@
+// KLASA KANBAN CARD
+
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var prefix = "https://cors-anywhere.herokuapp.com/";
+var myHeaders = {
+    'X-Client-Id': 3851,
+    'X-Auth-Token': '5905e6ef19a34924ae6c9414a9eb4386',
+};
+
+
+
+
+
+function Card(id, name) {
+    var self = this;
+
+    this.id = id;
+    this.name = name;
+    this.element = generateTemplate('card-template', {
+        description: this.name
+    }, 'li');
+
+    this.element.querySelector('.card').addEventListener('click', function (event) {
+        event.stopPropagation();
+
+        if (event.target.classList.contains('btn-delete')) {
+            self.removeCard();
+        }
+    });
+}
+Card.prototype = {
+    removeCard: function () {
+        var self = this;
+        fetch(prefix + baseUrl + '/card/' + self.id, {
+                method: 'DELETE',
+                headers: myHeaders
+            })
+            .then(function (resp) {
+                return resp.json();
+            })
+            .then(function (resp) {
+                self.element.parentNode.removeChild(self.element);
+            })
+    }
+}
